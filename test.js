78e977f710f591998435d52d70b71ff4b70a36a5
zhangzhen <1094705507@qq.com>
2020-03-26 13:38:23 +0800

    修改轮播图

--- test.js
+++ test.js
@@ -16,7 +16,135 @@ const certificate = fs.readFileSync(path.join(__dirname, "./2361522_bws666.com.p
 // 	pfx: fs.readFileSync(path.join(__dirname, "./apiclient_cert.p12"), "utf8"), //微信商户平台证书,
 // 	passphrase: 1537649941 // 商家id
 // };
-const credentials = {
+const credentials = {/* eslint-disable react-native/no-inline-styles */
+	import React from 'react';
+	import PayItem from './PayItem';
+	import MoneyItem from './MoneyItem';
+	import FastImage from '../component/FastImage';
+	import CommonHeader from '../component/CommonHeader';
+	import CommonStyle from '../style/common';
+	import {Text, View, StyleSheet, ScrollView, Dimensions} from 'react-native';
+	const {width} = Dimensions.get('window');
+	
+	export default class ReCharge extends React.Component {
+		constructor(props) {
+			super(props);
+			this.state = {
+				activeMoney: 1,
+				payWay: 'alipay',
+			};
+		}
+	
+		componentDidMount() {}
+	
+		// 支付金额改变
+		onPressChargeItem(key) {
+			this.setState({activeMoney: key});
+		}
+	
+		// 支付方式改变
+		payWayChange(key) {
+			this.setState({payWay: key});
+		}
+	
+		render() {
+			const {navigation} = this.props;
+			let {activeMoney, payWay} = this.state;
+			return (
+				<View style={styles.container}>
+					<CommonHeader title="充值" navigation={navigation} />
+					<ScrollView style={styles.content}>
+						<FastImage
+							style={styles.content_logo}
+							source={require('../../img/public/logo2.png')}
+						/>
+						<View style={styles.detail_common_title}>
+							<Text style={{fontSize: 14, color: '#333'}}>
+								余额充值
+							</Text>
+						</View>
+						<View style={styles.content_account}>
+							<MoneyItem
+								money={1000}
+								discount={200}
+								active={activeMoney === 1}
+								onPress={this.onPressChargeItem.bind(this, 1)}
+							/>
+							<MoneyItem
+								money={500}
+								discount={80}
+								active={activeMoney === 2}
+								onPress={this.onPressChargeItem.bind(this, 2)}
+							/>
+							<MoneyItem
+								money={200}
+								discount={20}
+								active={activeMoney === 3}
+								onPress={this.onPressChargeItem.bind(this, 3)}
+							/>
+							<MoneyItem
+								money={100}
+								active={activeMoney === 4}
+								onPress={this.onPressChargeItem.bind(this, 4)}
+							/>
+						</View>
+						<View style={styles.detail_common_title}>
+							<Text style={{fontSize: 14, color: '#333'}}>
+								选择支付方式
+							</Text>
+						</View>
+						<PayItem
+							iconName="alipay-circle"
+							onPress={this.payWayChange.bind(this, 'alipay')}
+							iconColor="#208ee9"
+							text="支付宝支付"
+							active={payWay === 'alipay'}
+						/>
+						<PayItem
+							iconName="wechat"
+							onPress={this.payWayChange.bind(this, 'wechat')}
+							iconColor="#89e04c"
+							text="微信支付"
+							active={payWay === 'wechat'}
+						/>
+					</ScrollView>
+					<View style={styles.bottom_btn}>
+						<Text style={styles.bottom_btn_text}>确认支付</Text>
+					</View>
+				</View>
+			);
+		}
+	}
+	
+	const styles = StyleSheet.create({
+		container: {
+			flex: 1,
+			backgroundColor: '#fff',
+		},
+		content: {
+			flex: 1,
+			// backgroundColor: 'red',
+			paddingHorizontal: 10,
+		},
+		content_logo: {
+			width: width - 20,
+			height: 0.4 * width,
+		},
+		detail_common_title: CommonStyle.detail_common_title,
+		detail_common_title: {
+			height: 20,
+			marginVertical: 10,
+			justifyContent: 'center',
+			paddingLeft: 10,
+			borderLeftColor: '#fb9dd0',
+			borderLeftWidth: 3,
+			marginBottom: 10,
+		},
+
+
+
+	});
+	
 	// pfx: pfx, //微信商户平台证书,
 	key: privateKey,
 	cert: certificate,
